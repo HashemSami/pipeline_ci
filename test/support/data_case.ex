f556c860c1a5d5ade68bd177cd8919f050ac4ec6
hashemsami <hashem.sf@gmail.com>
2024-03-12 20:05:14 +0300

    adding testing workflows

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -16,6 +16,8 @@ defmodule PipelineCi.DataCase do
 
   use ExUnit.CaseTemplate
 
+  alias Ecto.Adapters.SQL
+
   using do
     quote do
       alias PipelineCi.Repo
@@ -36,10 +38,13 @@ defmodule PipelineCi.DataCase do
   Sets up the sandbox based on the test tags.
   """
   def setup_sandbox(tags) do
-    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(PipelineCi.Repo, shared: not tags[:async])
-    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
+    pid = SQL.Sandbox.start_owner!(PipelineCi.Repo, shared: not tags[:async])
+    on_exit(fn -> SQL.Sandbox.stop_owner(pid) end)
   end
 
+  @spec errors_on(Ecto.Changeset.t()) :: %{
+          optional(atom()) => list() | %{optional(atom()) => any()}
+        }
   @doc """
   A helper that transforms changeset errors into a map of messages.
 
